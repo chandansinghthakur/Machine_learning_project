7a8fc653ed37673ba63a92819fe0bd9144a3ae16
chandansinghthakur <chandan216113@gmail.com>
2022-06-20 18:35:49 +0530

    update 2

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 from typing import List
 
 
@@ -8,14 +8,13 @@ PROJECT_NAME="housing-predictor"
 VERSION="0.0.1"
 AUTHOR="CHANDAN"
 DESCRIPTION="This is my first project"
-PACKAGES=["housing"]
 REQUIREMENTS_FILE_NAME="requirements.txt"
 
 
 def get_requirements_list()->List[str]:
     """Descripton: This function reads the requirements file and returns a list of requirements"""
     with open(REQUIREMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -23,7 +22,6 @@ name =PROJECT_NAME,
 version =VERSION,
 author =AUTHOR,
 description =DESCRIPTION,
-packages =PACKAGES,
+packages =find_packages(),
 install_requires =get_requirements_list()
 )
-
